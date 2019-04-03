aacda400ee6fb3b2242a4e30e3c907475dab2f4a
Roman Juranek <ijuranek@fit.vutbr.cz>
2019-04-03 16:19:45 +0200

    New nms function (to replace groupRectangles form opencv) and ROC computation

--- waldboost/groundtruth.py
+++ waldboost/groundtruth.py
@@ -4,7 +4,7 @@ from random import uniform, gauss
 import bbox
 
 
-def bb_to_xys(bb, f=20):
+def bb_to_xys(bb, f=3):
     x,y,w,h = bb
     return x+0.5*w, y+0.5*h, f*math.log2(w)
 
@@ -16,14 +16,27 @@ def bb_distance(bb0, bb1):
 
 
 def bb_overlap_distance(bb0, bb1):
-    raise NotImplementedError
+    x0,y0,w0,h0 = bb0
+    x1,y1,w1,h1 = bb1
+    xa = max(x0,x1)
+    ya = max(y0,y1)
+    xb = min(x0+w0,x1+w1)
+    yb = min(y0+h0,y1+h1)
+    #print(xa,xb,ya,yb)
+    if xa > xb or ya > yb:
+        return 1
+    i = (xb-xa) * (yb-ya)
+    u = (w0*h0) + (w1*h1) - i
+    #print(i,u)
+    return 1 - (i/u)
 
 
-def distance_matrix(dt, gt, metric=bb_distance):
+
+def distance_matrix(dt, gt, metric=bb_overlap_distance):
     return np.array( [[metric(g,d) for d in dt] for g in gt] )
 
 
-def match(dt, gt=None, allow_multiple=True, metric=bb_distance, default_dist=np.inf):
+def match(dt, gt=None, allow_multiple=True, metric=bb_overlap_distance):
     """
     Match detections (dt) to ground truth (gt) by using metric.
     """
@@ -32,9 +45,10 @@ def match(dt, gt=None, allow_multiple=True, metric=bb_distance, default_dist=np.
     assert isinstance(dt, np.ndarray)
 
     n_dt = dt.shape[0]
+    n_gt = gt.shape[0]
 
     if not has_gt or n_dt == 0:
-        gt_dist = []
+        gt_dist = np.full(n_gt, np.inf)
         dt_dist = np.full(n_dt, np.inf)
         dt_ign = np.zeros(n_dt, np.bool)
         return dt_dist, dt_ign, gt_dist
@@ -43,7 +57,7 @@ def match(dt, gt=None, allow_multiple=True, metric=bb_distance, default_dist=np.
     has_ign_flag = gt_cols == 5
     assert gt_cols in [4,5], "GT must have 4 or 5 columns"
     dist = distance_matrix(dt, gt[...,:4], metric=metric)
-    ign_flag = gt[...,4].astype(np.bool) if has_ign_flag else np.zeros(n_gt,np.bool)
+    ign_flag = gt[:,4].astype(np.bool) if has_ign_flag else np.zeros(n_gt,np.bool)
     # Todo use linear sum assignment when allow_multiple is false - distance of unassigned dt will be set to default_dist
     if allow_multiple:
         dt_dist = np.min(dist, axis=0)
@@ -55,7 +69,7 @@ def match(dt, gt=None, allow_multiple=True, metric=bb_distance, default_dist=np.
     return dt_dist, dt_ign, gt_dist
 
 
-def read_bbgt(filename, lbls={}, ilbls={}, ar=1, resize=1):
+def read_bbgt(filename, lbls={}, ilbls={}, ar_range=None, ar=1, resize=1):
     """
     Read ground truth from bbGt file.
     See Piotr's Toolbox for details
@@ -71,9 +85,13 @@ def read_bbgt(filename, lbls={}, ilbls={}, ar=1, resize=1):
             assert len(elms) == 12, "Invalid file"
             #print(elms)
             lbl = elms[0]
-            bb = tuple(map(int, elms[1:5]))
+            x,y,w,h = bb = tuple(map(int, elms[1:5]))
+            ar = w/h
             ign = bool(int(elms[10]))
             if lbl in lbls:
+                if ar_range is not None:
+                    a0,a1 = ar_range
+                    ign = not (a0 <= ar <= a1) or ign
                 bb = bbox.set_aspect_ratio(bb, ar=ar, type=bbox.KEEP_WIDTH)
                 bb = bbox.resize(bb, resize)
                 gt.append(bb + (ign,))
@@ -87,3 +105,25 @@ def read_bbgt(filename, lbls={}, ilbls={}, ar=1, resize=1):
     if gt.size == 0:
         gt = np.empty((0,5),"f")
     return gt
+
+
+def nms(bbs, score, min_group=1, distance=bb_overlap_distance, max_dist=0.7):
+    idx = np.argsort(score)[::-1]
+    groups = {}
+    suppressed = np.zeros(idx.size, np.bool)
+    for i,idx_i in enumerate(idx[:-1]):
+        if suppressed[idx_i]: continue
+        groups[i] = [idx_i]
+        for j,idx_j in enumerate(idx[i+1:]):
+            if suppressed[idx_j]: continue
+            if distance(bbs[idx_i], bbs[idx_j]) < max_dist:
+                groups[i].append(idx_j)
+                suppressed[idx_j] = True
+
+    res_bbs = []
+    res_score = []
+    for gid, group in groups.items():
+        res_bbs.append(np.mean(bbs[group], axis=0))
+        res_score.append(np.max(score[group]))
+
+    return np.array(res_bbs), np.array(res_score)
--- waldboost/testing.py
+++ waldboost/testing.py
@@ -1,58 +1,69 @@
+import cv2
 import logging
 import numpy as np
 from .detector import detect
-from .groundtruth import match, bb_overlap
+from .groundtruth import match, bb_overlap_distance, nms
+from sklearn import metrics
 
 
 logger = logging.getLogger(__name__)
 
 
 def process_images(testing_images, model, verifier=None):
-    dts = []
-    gts = []
-    for img, gt in testing_images:
-        dt, score = detect(img, model)
-        dt_overlap, dt_ign, gt_overlap = match(dt, gt, bb_overlap)
-        dts.append( list(zip(dt, score, dt_overlap, dt_ign)) )
-        gts.append( list(zip( gt, gt_overlap )) )
-
-    return gts, dts
+    results = {}
+    for img, gt, filename in testing_images:
+        dt, score, confidence = detect(img, model, verifier)
+        results[filename] = {
+            "dt": np.array(dt), "score": score, "confidence": confidence, "gt": np.array(gt)
+        }
+    return results
 
 
-def compute_roc(dt, gt, min_overlap=0.7, n_thresholds=1000):
-    # Gather all responses and determine their range
-    H = np.array( [d[1] for d in dt] )
-    h0 = min(H)
-    h1 = max(H)
+def nms_basic(dt, score, confidence):
+    mask = score>0
+    return nms(dt[mask,...], score[mask], max_dist=0.9)
 
-    # generate evaluation thresholds - regularly spaced in range of responses
-    T = np.linspace(h0, h1, n_thresholds)
 
-    # Initial counts of tp, fp and fn
-    n_tp = np.zeros(n_thresholds)
-    n_fp = np.zeros(n_thresholds)
-    n_fn = np.zeros(n_thresholds)
+def nms_with_verification(dt, score, confidence):
+    mask = np.logical_and(score>0, confidence>0.1)
+    return nms(dt[mask,...], score[mask], max_dist=0.9)
 
-    # Number of images (required since the function report FPPI)
-    n_imgs = len(dt)
 
-    # Go through the thresholds
-    for k,t in enumerate(T):
-        # Go through detections and count true positives and false posifives
-        for _, score, overlap, ign in dt:
-            tp = score>=t and ~ign and overlap>=min_overlap
-            fp = score>=t and ~ign and overlap<min_overlap
-            n_tp += tp.sum()
-            n_fp += fp.sum()
-        # Go through annotated objects and count false negatives
-        for gt, overlap in gt:
-            ign = gt[...,4]
-            fn = score>=t and ~ign and overlap<min_overlap
-            n_fn += fn.sum()
+def compute_roc(results, nms_func, metric=bb_overlap_distance, max_dist=0.5):
+    Y = []
+    score = []
+    ignore = []
+    gt_missed = 0
+    gt_total = 0
 
-    # Detection rate is the ration of true positives in true positives and false negatives (i.e. total number of objects)
-    det_rate = n_tp / (n_tp + n_fn)
-    # FPPI is average nubmer of false positives on each image
-    fppi_rate = n_fp / n_imgs
+    for f,r in results.items():
+        gt = r["gt"]
+        dt_nms, score_nms = nms_func(r["dt"], r["score"], r["confidence"])
+        dt_dist, dt_ign, gt_dist = match(dt_nms, gt, metric=metric)
+        has_ign_flag = gt.shape[1] == 5
+        ign_flag = gt[:,4].astype(np.bool) if has_ign_flag else np.zeros(n_gt,np.bool)
+        Y.append(dt_dist < max_dist)  # distance to some gt is less than threshold -> tp
+        score.append(score_nms)  # Detection score
+        ignore.append(dt_ign)
+        gt_missed += np.logical_and(gt_dist>=max_dist, ~ign_flag).sum()
+        gt_total += (~ign_flag).sum()
 
-    return 1-det_rate, fppi_rate, T
+    Y = np.concatenate(Y)
+    score = np.concatenate(score)
+    ignore = np.concatenate(ignore)
+
+    Y = Y[~ignore]
+    score = score[~ignore]
+    det_rate = 1-(gt_missed / gt_total)
+    n_imgs = len(results.keys())
+
+    print(gt_missed, gt_total, det_rate)
+
+    bins = np.linspace(score.min(), score.max(), 1000)
+    tp,_ = np.histogram(score[Y==1], bins)
+    fp,_ = np.histogram(score[Y==0], bins)
+    fppi = (fp.sum() - np.cumsum(fp)) / n_imgs
+    miss = 1-(1-(np.cumsum(tp) / tp.sum())) * det_rate
+
+
+    return fppi, miss, bins
