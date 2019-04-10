Fixed channel computation in fpga module and in channel_pyramid

waldboost/channels.py
@@ -93,4 +93,4 @@ def channel_pyramid(image, opts):
 
             yield np.atleast_3d(chns), real_scale/shrink
 
-        base_image = block_reduce(base_image, (2,2), np.min).astype(image.dtype)
+        base_image = block_reduce(base_image, (2,2), np.mean).astype(image.dtype)

waldboost/fpga.py
@@ -32,7 +32,9 @@ def grad_hist_4(image):
     D = np.array( [1,0,-1], "i2")
 
     im = convolve1d(image.astype("i2"),H,axis=0)
-    convolve1d(im,H,axis=1,output=im) >> 4  # back to 8 bits
+    convolve1d(im,H,axis=1,output=im)
+    im = np.right_shift(im, 4)
+
     gy = convolve1d(im, D, axis=0) >> 1
     gx = convolve1d(im, D, axis=1) >> 1
 