Old training code removed, fixed fpga training, acceleration of channel computation with numba

waldboost/__init__.py
@@ -1,26 +1,69 @@
 """ Waldboost package
+
+Support for training WaldBoost detectors
+
+Functions
+---------
+train                       Train new detector
+detect, detect_multiple     Detect objects on image
+load, load_model            Load model from a file
+save, save_model            Save model to file
+
+Classes
+-------
+DTree
+Learner
+Model
+Pool
+
+Example
+-------
+import waldboost as wb
+from waldboost.utils import fake_data_generator
+
+# Prepare parameters
+training_images = fake_data_generator()
+chns = wb.default_channel_opts
+shape = (14,14,1)  # size is arbitrary, but number of channels must correspond to what is in chns
+model = wb.Model(shape, chns)
+
+# Train the model
+stats = wb.train(model, training_images)
+# stats contain training loss and class probs for each training round
+
+# Draw next image from generator and detect
+image,*_ = next(training_images)
+bbs,scores = wb.detect(image, model)
+
+# bbs and scores contain locations of detected objects
 """
 
 
 import logging
+import numpy as np
+from collections import defaultdict
+from pkg_resources import resource_filename
+
 from .training import DTree, Learner
 from .samples import Pool
 from .model import Model
 from .channels import grad_mag, grad_hist
 from . import fpga, channels
-import numpy as np
 
 
-# with open(path.join(path.abspath(path.dirname(__file__)),"VERSION"),"r") as f:
-#     __version__ = f.read().strip()
+with open(resource_filename(__name__, "VERSION"), "r") as f:
+    __version__ = f.read().strip()
 
 
-load_model = Model.load
+load = load_model = Model.load
 
 
 def save_model(model, filename):
-    """ Save model. See Model.save """
+    """ Save model to file. See Model.save """
     model.save(filename)
+
+
+save = save_model
 
 
 def detect(image, model):
@@ -28,13 +71,110 @@ def detect(image, model):
     return model.detect(image)
 
 
-deafult_channel_opts = {
+def detect_multiple(image, *models, channel_opts=None, response_scale=None, separate=False):
+    """ Detect objects in image using multiple detectors (with shared channel options)
+
+    Inputs
+    ------
+    image : ndarray
+        Input image with shape (H,W) and np.uint8 dtype
+    models : waldboost.Model
+        Sequence of models to evaluate
+    channel_opts : dict
+        Channel options. By default models[0].channel_opts are used, but user
+        can specify their own. Usually they do not need to be modified, except
+        for channel_opts["n_per_oct"] parameter which controls number of scales
+        in pyramid.
+    separate : bool
+        When True, results for each model are returned as separate arrays,
+        otherwise they are merged together. Usually, when models are intended
+        to detect different classes, separate=True. When models detect the same
+        class (e.g. in different scales, or different kinds of class), then
+        separate=False.
+
+    Outputs
+    -------
+    bbs, scores : default_dict or ndarray
+        Bounding boxes of detected objects. In case of separate=True it is an
+        instance of default_dict where bbs[k] and scores[k], correspond to results
+        of models[k]. Otherwise it is ndarray with results merged form all models.
+        The results correspond to that of waldboost.detect.
+
+    See also
+    --------
+    waldboost.detect : Detection with single model
+
+    Notes
+    -----
+    In case of a single model (i.e. detect_multiple(image, model)) the result
+    is exactly equivalent to detect(image, model) and also to model.detect(image).
+
+    Example
+    -------
+    Multiple classes:
+        model_files = ["face.pbz", "car.pbz"]
+        models = [waldboost.load(f) for f in model_files]
+        image = imread("image.jpg")  # Need grayscale uint8
+        bbs, scores = detect_multiple(image, *models, separate=True)
+        # bbs[k] and scores[k] correspond to results of models[k]
+
+    Single class, multiple detectors:
+        model_files = ["scale_0.pbz", "scale_1.pbz"]
+        models = [waldboost.load(f) for f in model_files]
+        image = imread("image.jpg")  # Need grayscale uint8
+        # Modify how channels are calculated (if needed)
+        channels = models[0].channel_opts
+        channels["n_per_oct"] = 1
+        bbs, scores = detect_multiple(image, channel_opts=channels, eparate=False, *models)
+        # bbs and scores are merged results for all models
+
+    Also call with individual models as positional arguments (instead of list)
+    is possible:
+        detect_multiple(image, model_a, model_b, model_c, separate=True)
+    """
+    channel_opts = channel_opts or models[0].channel_opts
+    if response_scale is None:
+        response_scale = [1] * len(models)
+    response_scale = np.array(response_scale, "f")
+
+    bbs = defaultdict(list)
+    scores = defaultdict(list)
+    for chns, scale in channels.channel_pyramid(image, channel_opts):
+        # Generate channel pyramid from the input image
+        for k, model in enumerate(models):
+            # Evaluate each model on the channels
+            r,c,h = model.predict_on_image(chns)
+            bbs_k = model.get_bbs(r,c,scale)
+            if separate:
+                bbs[k].append(bbs_k)
+                scores[k].append(h)
+            else:
+                bbs[0].append(bbs_k)
+                scores[0].append(h)
+
+    for k in bbs:  # k is model key to bbs and scores
+        bbs_k = [x for x in bbs[k] if x.size]
+        scores_k = [x for x in scores[k] if x.size]
+        if bbs_k:
+            bbs[k] = np.concatenate(bbs_k)
+            scores[k] = np.concatenate(scores_k) * response_scale[k]
+    if not separate:
+        bbs, scores = bbs[0], scores[0]
+
+    return bbs, scores
+
+
+default_channel_opts = {
     "shrink": 2,
     "n_per_oct": 8,
     "smooth": 0,
     "target_dtype": np.float32,
     "channels": [ grad_mag ]
     }
+
+
+def _class_prob_cb(model, learner, stage):
+    print(f"{learner.P0}")
 
 
 def train(model,
@@ -45,9 +185,13 @@ def train(model,
           n_pos=1000,
           n_neg=1000,
           max_depth=2,
-          callbacks=[],
+          target_p0=1e-5,
+          callbacks=[_class_prob_cb],
           logger=None):
     """ Train or continue training detection model
+
+    This is a baseline training pipeline. It simply trains a classifier
+    of specified length using supplied training images.
 
     Inputs
     ------
@@ -70,8 +214,24 @@ def train(model,
         Number of positive (resp. negative) samples for stage training
     max_depth : scalar int
         Depth of decision trees in the model
+    target_p0 : scalar
+        Condition for stopping rejection of training samples. If 0 < stop_sampling < 1
+        (usually close to zero, like 1e-5), rejection stops when p0 (false positive
+        probability) reaches below this threshold. When stop_sampling>=1, rejection
+        stops at the specified stage.
     callbacks : list of functions
+        List of functions with signature: callback(model, stage, learner)
     logger : logging.Logger
+
+    Outputs
+    -------
+    stats : dict
+        Training statistics with keys "loss", "p0" and "p1" - all ndarray with
+        length corresponding to number of stages in the model.
+        loss - value of loss function
+        p0   - false positive rate. The ratio of background samples that make
+               it to the stage. The value should asymptotically decrease to 0
+        p1   - true positive rate. Asymptotically decreases to 1-alpha
 
     Example
     -------
@@ -96,17 +256,31 @@ def train(model,
     [1] Sochman et al.: Waldboost-learning for time constrained sequential detection, CVPR 2005
     """
 
+    def theta(n, p0):
+        assert target_p0 > 0, "target_p0 must be positive"
+        if 0 < target_p0 < 1:
+            theta = None if p0 > target_p0 else -np.inf
+        else:
+            theta = None if n < target_p0 else -np.inf
+        return theta
+
     logger = logger or logging.getLogger("WaldBoost")
 
     learner = Learner(alpha=alpha, wh=DTree, max_depth=max_depth, min_samples_leaf=10)
     pool = Pool(model.shape, min_tp=n_pos, min_fp=n_neg)
     pool.max_tp_dist = max_tp_dist
 
+    stats = defaultdict(list)
     for stage in range(1,length+1):
         logger.info(f"Training stage {stage}")
         pool.update(model, training_images)
         X0,H0 = pool.gather_samples(0)
         X1,H1 = pool.gather_samples(1)
-        learner.fit_stage(model, X0, H0, X1, H1)
+        loss,p0,p1 = learner.fit_stage(model, X0, H0, X1, H1, theta=theta(stage, learner.P0))
         for cb in callbacks:
-            cb(stage, model, learner)
+            cb(model, learner, stage)
+        stats["loss"].append(loss)
+        stats["p0"].append(p0)
+        stats["p1"].append(p1)
+
+    return stats

waldboost/channels.py
@@ -1,9 +1,10 @@
 import logging
 import numpy as np
+import numba as nb
 from skimage.measure import block_reduce
 from skimage.transform import resize
 from scipy.ndimage import convolve1d
-
+import cv2
 
 logger = logging.getLogger(__name__)
 
@@ -53,12 +54,47 @@ def grad_hist(image, n_bins=6, full=False):
         return np.abs(chns)
 
 
-def octaves(image):
-    dtype = image.dtype
+@nb.njit(nogil=True)
+def downscale(arr):
+    u = arr.shape[0]
+    v = arr.shape[1]
+    u_lim = u - (u%2)
+    v_lim = v - (v%2)
+    return ((arr[0:u_lim:2,0:v_lim:2,...] +
+            arr[1:u_lim:2,0:v_lim:2,...] +
+            arr[0:u_lim:2,1:v_lim:2,...] +
+            arr[1:u_lim:2,1:v_lim:2,...]) / 4).astype(arr.dtype)
+
+
+@nb.stencil(neighborhood=((-1,1),(-1,1)))
+def _smooth(arr):
+    v =   arr[-1,-1] + 2*arr[-1,0] +   arr[-1,1] + \
+        2*arr[ 0,-1] + 4*arr[ 0,0] + 2*arr[ 0,1] + \
+          arr[ 1,-1] + 2*arr[ 1,0] +   arr[ 1,1]
+    return v / 16
+
+
+@nb.njit(nogil=True)
+def _smooth_image(arr):
+    return _smooth(arr).astype(arr.dtype)
+
+
+@nb.njit(nogil=True)
+def smooth_image_3d(arr):
+    smoothed = np.empty_like(arr)
+    for k in nb.prange(arr.shape[2]):
+        smoothed[...,k] = _smooth(arr[...,k])
+    return smoothed
+
+
+def _image_octaves(image, min_size=(16,16)):
     base_image = image.copy()
     while True:
         yield base_image
-        base_image = block_reduce(base_image, (2,2,1), np.mean).astype(dtype)
+        h,w = base_image.shape[:2]
+        if ((w//2,h//2) < min_size):
+            break
+        base_image = downscale(base_image)
 
 
 def channel_pyramid(image, channel_opts):
@@ -67,38 +103,29 @@ def channel_pyramid(image, channel_opts):
     smooth = channel_opts["smooth"]
     channels = channel_opts["channels"]
     target_dtype = channel_opts["target_dtype"]
-
-    assert shrink in [1,2,3,4], "Shrink factor must be integer 1 <= shrink <= 4"
-
-    base_image = image
+    assert shrink in [1,2], "Shrink factor must be integer 1 <= shrink <= 2"
 
     factor = 2**(-1/n_per_oct)
-
-    while True:
+    for base_image in _image_octaves(image):
         h,w,*_ = base_image.shape
         for i in range(n_per_oct):
             s = factor ** i
             nw, nh = int((w*s)/shrink)*shrink, int((h*s)/shrink)*shrink
-            if nh < 50 or nw < 50:
-                return
-
             real_scale = nw / image.shape[1]
-            im = (255*resize(base_image, (nh, nw))).astype("u1")
+
+            im = cv2.resize(base_image, (nw, nh), cv2.INTER_LINEAR)[...,None]
 
             if channels:
-                chns = [ func(im[...,0]) for func in channels ]
+                chns = [func(im[...,0]) for func in channels]
                 chns.append(im[...,1:])
+                chns = np.concatenate(chns, axis=-1)
             else:
-                chns = [ im[...,None] ]
+                chns = im
 
-            chns = np.concatenate(chns, axis=-1)
-            if shrink > 1:
-                chns = block_reduce(chns, (shrink,shrink,1), np.mean).astype(target_dtype)
+            if shrink == 2:
+                chns = downscale(chns)
 
-            if smooth > 0:
-                H = triangle_kernel(smooth)
-                chns = separable_convolve(chns, H)
+            if smooth == 1:
+                chns = smooth_image_3d(chns)
 
             yield np.atleast_3d(chns), real_scale/shrink
-
-        base_image = block_reduce(base_image, (2,2,1), np.mean).astype(image.dtype)

waldboost/fpga.py
@@ -1,16 +1,19 @@
 """
-Support for FPGA stuff - like feature banks, quantization etc.
+Support for FPGA stuff.
 """
 
 import logging
 import numpy as np
 from scipy.ndimage import convolve1d
-from .training import DTree, as_features
 from itertools import count
 from queue import Queue
+from collections import deque, defaultdict
+
+from .training import DTree, Learner, as_features
+from .samples import Pool
 
 
-def bank_pattern(shape, block_shape=(2,2)):
+def _bank_pattern(shape, block_shape):
     assert len(shape) in [2,3], "Shape must be (H,W) or (H,W,C)"
     if len(shape) == 2:
         shape += (1,)
@@ -20,6 +23,23 @@ def bank_pattern(shape, block_shape=(2,2)):
     u,v,c = shape
     banks = np.atleast_3d(np.dstack([banks]*c))
     return banks[:u,:v,...]
+
+
+class PixelBanks:
+    def __init__(self, shape, block_shape):
+        self.pattern = _bank_pattern(shape, block_shape)
+
+    def bank_pixels(self, bank_ids):
+        return np.concatenate( [np.flatnonzero(self.pattern == b) for b in bank_ids] )
+
+
+class BankScheduler:
+    def __init__(self, n_banks=8):
+        self.n_banks = n_banks
+        self.bank_counter = count()
+
+    def schedule(self, max_depth=2):
+        return [ [next(self.bank_counter) % self.n_banks] for _ in range(max_depth)]
 
 
 def grad_hist_4(image):
@@ -33,70 +53,101 @@ def grad_hist_4(image):
     H = np.array( [1,2,1], "i2")
     D = np.array( [1,0,-1], "i2")
 
-    im = convolve1d(image.astype("i2"),H,axis=0)
-    convolve1d(im,H,axis=1,output=im)
-    im = np.right_shift(im, 4)
+    im = image.astype("i2")
 
-    gy = convolve1d(im, D, axis=0) >> 1
-    gx = convolve1d(im, D, axis=1) >> 1
+    gy = convolve1d(convolve1d(im,H,axis=1), D, axis=0) / 4
+    gx = convolve1d(convolve1d(im,H,axis=0), D, axis=1) / 4
 
     chns = np.empty(im.shape + (4,), "i2")
 
     theta = np.linspace(0, np.pi, 5)
     cs = np.cos(theta[:-1])
     sn = np.sin(theta[:-1])
-
     for i,(c,s) in enumerate(zip(cs,sn)):
         chns[...,i] = gx*c - gy*s;
 
     return np.abs(chns)
 
 
-# def fit_feature(x,y,w):
-#     f = np.random.randint(x.shape[1])
-#     t = np.median(x[:,f])
-#     return f, t
+def H(*p):
+    tot = sum(p)
+    return -sum((_p/tot) * np.log2(_p/tot) for _p in p)
 
 
-def gini(w0, w1):
-    return 1 - (w0**2 + w1**2)
-
-
-def entropy(w0, w1):
-    return -((w0*np.log2(w0)) + (w1*np.log2(w1)))
-
-
-def fit_threshold(v0, v1, w0, w1, vmin, vmax):
-    # print("values:", v0.shape, v1.shape)
-    # print("weights:", w0.shape, w1.shape)
-    # print("range:", vmin, vmax)
-    bins = np.r_[vmin-1:vmax+1]
-    h0,_ = np.histogram(v0, bins, weights=w0)
-    h1,_ = np.histogram(v1, bins, weights=w1)
-    h0 = np.cumsum(h0)
-    h1 = np.cumsum(h1)
-    w = h0 + h1
-    h0_max = h0[-1]
-    h1_max = h1[-1]
-    h_max = h0_max + h1_max
-    metric = entropy(h0_max,h1_max) - (w/h_max)*entropy(h0,h1) - ((h_max-w)/h_max)*entropy(h0_max-h0,h1_max-h1)
-    k = np.argmin(metric)
+def _fit_threshold(x0,x1,w0,w1,xmin,xmax):
+    """ Find threshold to split v0 and v1 using information gain """
+    bins = np.arange(xmin,xmax+1)
+    p0,_ = np.histogram(x0, bins, weights=w0)
+    p1,_ = np.histogram(x1, bins, weights=w1)
+    l0 = np.cumsum(p0)
+    l1 = np.cumsum(p1)
+    l0_tot = l0[-1]
+    l1_tot = l1[-1]
+    r0 = l0_tot - l0
+    r1 = l1_tot - l1
+    left_w = (l0+l1) / (l0_tot + l1_tot)
+    right_w = (r0+r1) / (l0_tot + l1_tot)
+    metric = H(l0_tot, l1_tot) - (left_w*H(l0+1e-4,l1+1e-4) + right_w * H(r0+1e-4,r1+1e-4))
+    k = np.argmax(metric)
     return bins[k+1], metric[k]
 
 
-def fit_feature(x, y, w):
-    n_samples, n_features = x.shape
-    t_min,t_max = x.min(axis=0), x.max(axis=0)
-    x0,w0 = x[y==0].T,w[y==0]
-    x1,w1 = x[y==1].T,w[y==1]
-    threshold, metric = zip(*[fit_threshold(v0,v1,w0,w1,t0,t1) for v0,v1,t0,t1 in zip(x0,x1,t_min,t_max)])
+def _find_split(x, y, w):
+    """ Find best split for the data
+    Inputs
+    ------
+    x : ndarray
+        Samples (n_samples, n_features)
+    y : ndarray
+        Label (0, 1)
+    w : ndarray
+        Sample weights
+    """
+
+    x0,w0 = np.ascontiguousarray(x[y==0]).astype("f"), np.ascontiguousarray(w[y==0])
+    x1,w1 = np.ascontiguousarray(x[y==1]).astype("f"), np.ascontiguousarray(w[y==1])
+    xmin,xmax = x.min(axis=0), x.max(axis=0)
+
+    threshold, metric = zip(*[_fit_threshold(_x0, _x1, w0, w1, _xmin, _xmax) for _x0,_x1,_xmin,_xmax in zip(x0.T, x1.T,xmin,xmax)])
+
     k = np.argmax(metric)
     return k, threshold[k], metric[k]
 
 
 class FPGA_DTree:
+    """ Decision tree training algorithm for FPGA
+    The difference from waldboost.training.DTree (based on sklearn):
+    * Features in split nodes can be restricted according to node depth using
+      user-defined function. This is required by FPGA in order to avoid
+      colisions while evaluating trees in parallel.
+
+    waldboost.training.DTree and waldboost.fpga.DTree can be used interchangably
+    though the former has more options due to sklearn implementation.
+    """
     @staticmethod
-    def fit(X0, W0, X1, W1, max_depth=2):
+    def fit(X0, W0, X1, W1, max_depth=2, min_samples_leaf=10, allowed_features=None, clip=3, quantizer=32):
+        """ Train decision tree
+
+        Inputs
+        ------
+        X0, X1 : ndarray
+            Samples
+        W0, W1 : ndarray or None
+            Sample weights
+        max_depth : scalar int
+            Maximal depth of decision tree
+        min_samples_leaf : scalar int/float
+            TODO
+        allowed_features : callable
+            TODO
+
+        Outputs
+        -------
+        tree : waldboost.training.DTree
+            Initialized instance of decision tree. tree.apply, tree.predict and
+            others can be called.
+
+        """
         shape = X0.shape[1:]
 
         X = np.concatenate( [as_features(X0), as_features(X1)] )
@@ -104,27 +155,30 @@ class FPGA_DTree:
         W = np.concatenate( [W0, W1] )
 
         n_samples = W.size
-        W[Y==0] /= W[Y==0].sum() * 2
-        W[Y==1] /= W[Y==1].sum() * 2
-        #W /= W.sum()
-        print(W.sum(), W[Y==0].sum(), W[Y==1].sum())
+        _W = W.copy()
+        _W[Y==0] /= _W[Y==0].sum() * 2
+        _W[Y==1] /= _W[Y==1].sum() * 2
 
-        node_id = count()
-        sample_queue = Queue()
-        sample_queue.put( (np.arange(n_samples), 0, next(node_id)) )
+        node_id = count()  # Counter of node ids
+        sample_queue = Queue()  # Queue holding sample sets that need to be processed
+        sample_queue.put( (np.arange(n_samples), 0, next(node_id)) )  # Initial set includes all samples, depth is 0 and id is 0 (from the counter)
         nodes = dict() # Intermediate representation of tree
         while not sample_queue.empty():
             self_samples, depth, self_index = sample_queue.get()
-
             # Create either leaf or split node
-            create_leaf = (depth == max_depth) or (self_samples.size < 10)
+            create_leaf = (depth == max_depth) or (self_samples.size < min_samples_leaf)
             if create_leaf:
                 nodes[self_index] = {"samples": self_samples,
                                      "feature": -1, "threshold": -1,
                                      "left": -1, "right": -1}
                 # logging.info(f"Leaf node {nodes[self_index]}")
             else:
-                feature, threshold, _ = fit_feature(X[self_samples], Y[self_samples], W[self_samples])
+                ftrs = allowed_features[depth] if allowed_features is not None else np.r_[:X.shape[1]]
+                #print(f"Using {ftrs.size} features")
+                _X = X[:,ftrs]
+                feature, threshold, _ = _find_split(_X[self_samples], Y[self_samples], _W[self_samples])
+                feature = ftrs[feature]
+                # TODO: check for feature/metric feasibility
                 # split data
                 bin = X[self_samples, feature] <= threshold
                 # schedule l/r nodes
@@ -137,6 +191,7 @@ class FPGA_DTree:
                                      "feature": feature, "threshold": threshold,
                                      "left": left_index, "right": right_index}
 
+        # Compose final structude
         n_nodes = len(nodes)
         feature = [None] * n_nodes
         threshold = np.empty(n_nodes)
@@ -152,13 +207,22 @@ class FPGA_DTree:
             right[node_idx] = node_data["right"]
             idx = node_data["samples"]
             y, w = Y[idx], W[idx]
-            w0 = w[y==0].sum() + 1e-4
-            w1 = w[y==1].sum() + 1e-4
-            pred[node_idx] = np.round(32 * 0.5 * np.log(w1/w0)) / 32
+            w0 = w[y==0].sum() + 1e-3
+            w1 = w[y==1].sum() + 1e-3
+            pred[node_idx] = np.log(w1 / w0) / 2
 
-        #print(feature, threshold, left, right, pred)
+        if clip is not None:
+            pred = np.clip(pred, -clip, clip)
 
-        return DTree(feature, threshold, left, right, pred)
+        if quantizer is not None:
+            pred = np.round(quantizer*pred) / quantizer
+
+        # Return initialized waldboost.training.DTree instance
+        tree = DTree(feature, threshold, left, right, pred)
+
+
+
+        return tree
 
 
 channel_opts = {
@@ -168,3 +232,79 @@ channel_opts = {
     "target_dtype": np.int16,
     "channels": [ grad_hist_4 ],
     }
+
+
+def train(model,
+          training_images,
+          length=64,
+          alpha=0.2,
+          max_tp_dist=0.25,
+          n_pos=1000,
+          n_neg=1000,
+          max_depth=2,
+          target_p0=1e-5,
+          bank_pattern_shape=(2,2),
+          quantizer=32,
+          callbacks=[],
+          logger=None):
+    """ Train new model with FPGA friendly feature access patterns
+
+    The function mimics behaviour of waldboost.train.
+
+    Inputs
+    ------
+    Inputs have exactly same meaning as in waldboost.train
+    bank_pattern_shape : tuple
+        Size of pattern for bank scheduling. E.g. (2,2) means that there will
+        be 4 banks in 2x2 block.
+    quantizer : int
+        Model response multiplier. The responses and rejection thresholds are
+        still trained as floats, but when multiplied with quantizer value,
+        they are integers. So the model (after transformation) can be evaluated
+        with integer arithmentic.
+
+    Outputs
+    -------
+    stats : dict
+        Same as in waldboost.train
+
+    See also
+    --------
+    waldboost.train     Basline training pipeline
+
+    """
+
+    def theta(n, p0):
+        assert target_p0 > 0, "target_p0 must be positive"
+        if 0 < target_p0 < 1:
+            theta = None if p0 > target_p0 else -np.inf
+        else:
+            theta = None if n < target_p0 else -np.inf
+        return theta
+
+    logger = logger or logging.getLogger("WaldBoost/FPGA")
+
+    learner = Learner(alpha=alpha, wh=FPGA_DTree, max_depth=max_depth, min_samples_leaf=10, quantizer=quantizer)
+    pool = Pool(model.shape, min_tp=n_pos, min_fp=n_neg)
+    pool.max_tp_dist = max_tp_dist
+
+    banks = PixelBanks(model.shape, bank_pattern_shape)
+    scheduler = BankScheduler(np.prod(bank_pattern_shape))
+
+    stats = defaultdict(list)
+
+    for stage in range(1,length+1):
+        logger.info(f"Training stage {stage}")
+        pool.update(model, training_images)
+        X0,H0 = pool.gather_samples(0)
+        X1,H1 = pool.gather_samples(1)
+        stage_banks = scheduler.schedule(max_depth)
+        ftrs = [banks.bank_pixels(b) for b in stage_banks]
+        loss,p0,p1 = learner.fit_stage(model, X0, H0, X1, H1, allowed_features=ftrs, theta=theta(stage,learner.P0))
+        for cb in callbacks:
+            cb(model, learner, stage)
+        stats["loss"].append(loss)
+        stats["p0"].append(p0)
+        stats["p1"].append(p1)
+
+    return stats

waldboost/model.py
@@ -6,20 +6,22 @@ waldboost.model.Model
 """
 
 
-import numpy as np
-from .channels import channel_pyramid
-from . import model_pb2
-from .training import DTree
-import waldboost
 import zlib
+import logging
+import numpy as np
 from google.protobuf.message import DecodeError
+
+from . import model_pb2
+from .channels import channel_pyramid
+from .training import DTree
 
 
 def symbol_name(s):
     return s.__module__ + "." + s.__qualname__
 
 
-def symbol_from_name(name, custom_objects={}):
+def symbol_from_name(name):
+    print(f"Initializing symbol {name}")
     from importlib import import_module
     m,rest = name.split(".",1)
     ls = {m: import_module(m)}
@@ -60,6 +62,18 @@ class Model:
         self.shape = shape
         self.channel_opts = channel_opts
         self.classifier = []
+        self.n_loc = 0
+        self.n_weak = 0
+
+    def stats(self):
+        return self.n_weak/self.n_loc if self.n_loc>=0 else 0
+
+    def reset_stats(self):
+        self.n_loc = 0
+        self.n_weak = 0
+
+    def __getitem__(self, i):
+        return self.classifier[i]
 
     def channels(self, image):
         """ Iterator over channel pyramid of the given image
@@ -135,6 +149,7 @@ class Model:
         """
         bbs,scores = [],[]
         for chns, scale in self.channels(image):
+            #print(chns.shape, scale)
             r,c,h = self.predict_on_image(chns)
             bbs.append(self.get_bbs(r,c,scale))
             scores.append(h)
@@ -208,8 +223,11 @@ class Model:
         rs = idx % (u-m)
         cs = idx // (u-m)
         hs = np.zeros_like(rs, np.float32)
+        self.n_loc += hs.size  # Stats
         for weak, theta in self.classifier:
+            if not rs.size: break
             hs += weak.predict_on_image(X, rs, cs)
+            self.n_weak += hs.size  # Stats
             if theta == -np.inf:
                 continue
             mask = hs >= theta
@@ -262,38 +280,23 @@ class Model:
         for weak,theta in self.classifier:
             w_pb = proto.classifier.add()
             w_pb.theta = theta
-            # if isinstance(weak, DTree):
-            #     weak.as_proto(w_pb.dtree)
-            # if isinstance(weak, DStump):
-            #     weak.as_proto(w_pb.dstump)
-            # #if isinstance(weak, SKLearnDTree):
             weak.as_proto(w_pb.dtree1)
 
     @staticmethod
-    def from_proto(proto, custom_objects={}):
+    def from_proto(proto):
         """ Create new instance from model_pb2.Model """
         shape = tuple(proto.shape)
         channel_opts = {
             "shrink": proto.channel_opts.shrink,
             "n_per_oct": proto.channel_opts.n_per_oct,
             "smooth": proto.channel_opts.smooth,
-            "target_dtype": symbol_from_name(proto.channel_opts.target_dtype, custom_objects),
-            "channels": [ symbol_from_name(s, custom_objects) for s in proto.channel_opts.func ],
+            "target_dtype": symbol_from_name(proto.channel_opts.target_dtype),
+            "channels": [ symbol_from_name(s) for s in proto.channel_opts.func ],
         }
         M = Model(shape, channel_opts)
         for weak_proto in proto.classifier:
             theta = weak_proto.theta
             weak = DTree.from_proto(weak_proto.dtree1)
-            #tp = weak_proto.WhichOneof("weak")
-            # print(tp)
-            # if tp == "dtree":
-            #     weak = DTree.from_proto(weak_proto.dtree)
-            # elif tp == "dstump":
-            #     weak = DStump.from_proto(weak_proto.dstump)
-            # elif tp == "dtree1":
-            #     weak = SKLearnDTree.from_proto(weak_proto.dtree1)
-            # else:
-            #     print(f"Unknown type {tp}")
             M.append(weak, theta)
         return M
 
@@ -308,7 +311,7 @@ class Model:
             f.write(data)
 
     @staticmethod
-    def load(filename, custom_objects={}):
+    def load(filename):
         """ Load model from protobuf file and return new instance """
         with open(filename, "rb") as f:
             data = f.read()
@@ -317,47 +320,5 @@ class Model:
             proto.ParseFromString(data)
         except DecodeError:
             data = zlib.decompress(data)
-            proto.ParseFromString(data)
-        return Model.from_proto(proto, custom_objects)
-
-
-# from numba import jit
-#
-# class DTreeModel:
-#     def __init__(model):
-#         self.model = model;
-#         self.T = len(self.model)
-#         self.n = 3
-#         self.ftr = np.empty((T,n,3), "i")
-#         self.thr = np.empty((T,n), "f")
-#         self.h = np.empty((T,n,2), "f")
-#         self.theta = np.empty(t, "f")
-#         self.leaf = np.empty((T,n), "b")
-#         for t, (weak, theta) in enumerate(self.model.classifier):
-#             assert(isinstance(weak, DTree))
-#             self.theta[t] = theta
-#             for j in range(n):
-#                 self.ftr[t,j,:] = weak.nodes[j].ftr
-#                 self.thr[t,j] = weak.nodes[j].thr
-#                 self.h[t,j,:] = weak.nodes[j].h
-#                 self.leaf[]
-#
-#     @jit
-#     def predict_on_image(X):
-#         h,w = x.shape[:2]
-#         res = []
-#         for y in range(h):
-#             for x in range(w):
-#                 h = 0
-#                 for t in range(self.T): # weak loop
-#                     node = 0
-#                     while self.leaf[t,node]:
-#                         r,c,ch = self.ftr[t, node]
-#                         bin = X[y+r,x+c,ch] > self.thr[node]
-#                         node = 2*node + bin
-#                     h += self.h[t,node]
-#                     if h < self.theta[t]:
-#                         break
-#                 else:
-#                     res.append( (y,x,h) )
-#         return res
+        proto.ParseFromString(data)
+        return Model.from_proto(proto)

waldboost/samples.py
@@ -80,8 +80,8 @@ class Pool:
                     r_tp,c_tp,h_tp = sample_from_bbs(chns, detector.shape, scale_gt)
                     dt_tp = detector.get_bbs(r_tp, c_tp, scale)
                     real_tp,_ = self.classify_dt(dt_tp, gt)
-                    if len(real_tp) > 100:
-                         real_tp = np.random.choice(real_tp, 100)
+                    if len(real_tp) > 20:
+                         real_tp = np.random.choice(real_tp, 20)
                     r = np.concatenate( [r, r_tp[real_tp]] )
                     c = np.concatenate( [c, c_tp[real_tp]] )
                     h = np.concatenate( [h, h_tp[real_tp]] )

waldboost/training.py
@@ -1,7 +1,4 @@
-""" WaldBoost training algortithm
-
-
-"""
+""" WaldBoost training algortithm """
 
 
 import logging
@@ -12,200 +9,8 @@ from sklearn.tree import DecisionTreeClassifier
 logger = logging.getLogger(__name__)
 
 
-# def iter_features(X):
-#     """
-#     Sequence of X[:,a,b,c,...]
-#     """
-#     n,*shape = X.shape
-#     _X = X.reshape(n,-1)
-#     for k in range(_X.shape[-1]):
-#         yield _X[...,k]
-
-
 def weights(H):
     return np.exp(H) / H.size / 2
-
-
-# def normalized_weights(W0, W1):
-#     w = W0.sum() + W1.sum()
-#     return W0/w, W1/w
-
-
-# def loss(H0, H1):
-#     w0, w1 = normalized_weights(weights(H0), weights(-H1))
-#     return (w0.sum() + w1.sum()) / (H0.size+H1.size)
-
-
-# def weighted_sampling(n, p, trim_ratio=0.5):
-#     assert 0 <= trim_ratio <= 1, "Trim ratio must be in <0,1> interval"
-#     order = np.argsort(p)[::-1]
-#     n_trim = round(n * trim_ratio)
-#     trimmed, rest = np.split(order, [n_trim])
-#     p_rest = p[rest] / p[rest].sum()
-#     selected = np.random.choice(rest, n-n_trim, p=p_rest, replace=False)
-#     return np.concatenate([trimmed, selected])
-#
-#
-# def find_threshold(f0, w0, f1, w1):
-#     """
-#     Find threshold to separate two distributions
-#     """
-#     f = np.concatenate([f0,f1])
-#     fmin = np.min(f)
-#     fmax = np.max(f)
-#     # logger.debug(f"f: {f.size}, {f.dtype}, <{fmin},{fmax}>")
-#     if issubclass(f.dtype.type, np.integer):
-#         edges = np.r_[fmin-1:fmax+2]
-#     else:
-#         assert issubclass(f.dtype.type, np.floating)
-#         edges = np.linspace(fmin, fmax+1e-3, 256)
-#     # logger.debug(f"edges: {edges}")
-#     p0,_ = np.histogram(f0, bins=edges, weights=w0)
-#     p1,_ = np.histogram(f1, bins=edges, weights=w1)
-#     cdf0 = np.cumsum(p0)
-#     cdf1 = np.cumsum(p1)
-#     err = 2 * np.sqrt(cdf0*cdf1 + (1-cdf0)*(1-cdf1))
-#     k = np.argmin(err)
-#     return edges[k+1]
-#
-#
-# def fit_decision_stump(f0, w0, f1, w1, eps=1e-4, min_split_ratio=0.05):
-#     w0 = w0 / w0.sum()
-#     w1 = w1 / w1.sum()
-#     thr = find_threshold(f0, w0, f1, w1)
-#     wsum0,_ = np.histogram((f0<thr).astype(np.uint8), bins=[0,1,2], weights=w0)
-#     wsum1,_ = np.histogram((f1<thr).astype(np.uint8), bins=[0,1,2], weights=w1)
-#     hs = 0.5 * np.log((wsum1+eps) / (wsum0+eps))
-#     Z = 2 * np.sqrt(wsum0 * wsum1).sum()
-#     if min_split_ratio is not None:
-#         min_n = (f0.size + f1.size) * min_split_ratio
-#         if ((f0<thr).sum() + (f1<thr).sum()) < min_n or ((f0>=thr).sum() + (f1>=thr).sum()) < min_n:
-#             Z = np.inf # Penalize divergent solutions
-#     return (thr, hs), Z
-#
-#
-# class DStump:
-#     def __init__(self, ftr=None, thr=None, h=None, err=None):
-#         self.ftr = ftr
-#         self.thr = thr
-#         self.h = h
-#         self.err = err
-#     @staticmethod
-#     def fit(X0, W0, X1, W1):
-#         assert X0.shape[1:] == X1.shape[1:]
-#         shape = X0.shape[1:]
-#         instance = None
-#         err = None
-#         for k,(x0,x1) in enumerate(zip(iter_features(X0),iter_features(X1))):
-#             (thr, hs), err_k = fit_decision_stump(x0,W0,x1,W1)
-#             if err is None or err_k < err:
-#                 ftr = np.unravel_index(k, shape)
-#                 err = err_k
-#                 instance = DStump(ftr, thr, hs, err)
-#         return instance
-#     def quantize(self, func):
-#         self.h = func(self.h)
-#     @staticmethod
-#     def from_tuple(t):
-#         return DStump(*t)
-#     def as_tuple(self):
-#         return self.ftr, self.thr, self.h, self.err
-#     @staticmethod
-#     def from_proto(proto):
-#         return DStump(tuple(proto.ftr), proto.thr, np.array(proto.pred), proto.err)
-#     def as_proto(self, proto):
-#         proto.ClearField("ftr")
-#         proto.ClearField("pred")
-#         proto.ftr.extend(self.ftr)
-#         proto.thr = self.thr
-#         proto.pred.extend(self.h)
-#         proto.err = self.err
-#     def predict(self, X):
-#         self.check_trained()
-#         return self.h[self.predict_bin(X)]
-#     def predict_bin(self, X):
-#         self.check_trained()
-#         r, c, ch = self.ftr
-#         return (X[:,r,c,ch]<self.thr).astype(np.uint8)
-#     def predict_bin_on_image(self, X, rs, cs):
-#         self.check_trained()
-#         r, c, ch = self.ftr
-#         chs = np.full_like(rs, ch)
-#         return (X[rs+r,cs+c,chs]<self.thr).astype(np.uint8)
-#     def predict_on_image(self, X, rs, cs):
-#         return self.h[self.predict_bin_on_image(X, rs, cs)]
-#     def check_trained(self):
-#         assert self.ftr is not None, "Run fit() first"
-#
-#
-# class DTree:
-#     def __init__(self, nodes=None, leaf_tag=None):
-#         self.nodes = nodes
-#         self.leaf_tag = leaf_tag
-#     @staticmethod
-#     def fit(X0, W0, X1, W1, depth=2):
-#         n_nodes = (2**depth-1)
-#         nodes = [None] * n_nodes
-#         leaf_tag = [None] * n_nodes
-#         def fit_node(X0, W0, X1, W1, level, position):
-#             node = DStump.fit(X0, W0, X1, W1)
-#             nodes[position-1] = node
-#             leaf_tag[position-1] = level >= depth
-#             if level < depth:
-#                 b0 = node.predict_bin(X0)
-#                 b1 = node.predict_bin(X1)
-#                 fit_node(X0[b0==0], W0[b0==0], X1[b1==0], W1[b1==0], level+1, position=2*position+0)
-#                 fit_node(X0[b0==1], W0[b0==1], X1[b1==1], W1[b1==1], level+1, position=2*position+1)
-#         fit_node(X0, W0, X1, W1, level=1, position=1)
-#         return DTree(nodes, leaf_tag)
-#     def quantize(self, func):
-#         for n in self.nodes:
-#             n.quantize(func)
-#     @staticmethod
-#     def from_tuple(t):
-#         nodes, leaf_tag = t
-#         nodes = [DStump.from_tuple(n) for n in nodes]
-#         return DTree(nodes, leaf_tag)
-#     def as_tuple(self):
-#         return [n.as_tuple() for n in self.nodes], self.leaf_tag
-#     @staticmethod
-#     def from_proto(proto):
-#         nodes = [ DStump.from_proto(p) for p in proto.nodes ]
-#         leaf_tag = np.array(proto.leaf_tag)
-#         return DTree(nodes, leaf_tag)
-#     def as_proto(self, proto):
-#         proto.ClearField("nodes")
-#         proto.ClearField("leaf_tag")
-#         proto.leaf_tag.extend(self.leaf_tag)
-#         for n in self.nodes:
-#             n_pb = proto.nodes.add()
-#             n.as_proto(n_pb)
-#     def predict(self, X):
-#         def predict_node(X, node_id):
-#             if self.leaf_tag[node_id-1]:
-#                 return self.nodes[node_id-1].predict(X)
-#             else:
-#                 b = self.nodes[node_id-1].predict_bin(X)
-#                 h = np.empty(X.shape[0], "f")
-#                 h[b==0] = predict_node(X[b==0], 2*node_id+0)
-#                 h[b==1] = predict_node(X[b==1], 2*node_id+1)
-#                 return h
-#         self.check_trained()
-#         return predict_node(X, 1)
-#     def predict_on_image(self, X, rs, cs):
-#         def predict_node(rs, cs, node_id):
-#             if self.leaf_tag[node_id-1]:
-#                 return self.nodes[node_id-1].predict_on_image(X, rs, cs)
-#             else:
-#                 b = self.nodes[node_id-1].predict_bin_on_image(X, rs, cs)
-#                 h = np.empty(rs.size, np.float32)
-#                 h[b==0] = predict_node(rs[b==0], cs[b==0], 2*node_id+0)
-#                 h[b==1] = predict_node(rs[b==1], cs[b==1], 2*node_id+1)
-#                 return h
-#         self.check_trained()
-#         return predict_node(rs, cs, 1)
-#     def check_trained(self):
-#         assert all(self.nodes), "Run fit() first"
 
 
 def as_features(X):
@@ -261,8 +66,7 @@ class DTree:
         proto.right.extend(self.right)
         proto.prediction.extend(self.prediction)
     def apply(self, X):
-        n = X.shape[0]
-        node = np.zeros(n, "i")
+        node = np.zeros(X.shape[0], "i")
         for n,(ftr,t,lnode,rnode) in enumerate(zip(self.feature, self.threshold, self.left, self.right)):
             if ftr is None: continue
             r,c,ch = ftr
@@ -274,8 +78,7 @@ class DTree:
     def predict(self, X):
         return self.prediction[self.apply(X)]
     def predict_on_image(self, X, rs, cs):
-        n = rs.size
-        node = np.zeros(n,"i")
+        node = np.zeros(rs.size,"i")
         for n,(ftr,t,lnode,rnode) in enumerate(zip(self.feature, self.threshold, self.left, self.right)):
             if ftr is None: continue
             r,c,ch = ftr
@@ -285,7 +88,6 @@ class DTree:
             node[idx[~bin]] = rnode
         return self.prediction[node]
 
-#
 # def sample_training_data(X, H, W, n_samples=None, trim_ratio=0.2):
 #     if n_samples and n_samples < H.size:
 #         idx = weighted_sampling(n_samples, p=W/W.sum(), trim_ratio=trim_ratio)
@@ -295,20 +97,28 @@ class DTree:
 #     return _X, _H
 
 
+def loss(H0, H1):
+    W0 = weights(H0)
+    W1 = weights(-H1)
+    return W0.mean() + W1.mean()
+
+
 class Learner:
-    def __init__(self, alpha=0.1, n_samples=None, logger=None, wh=DTree, **wh_args):
+    """ Training algorithm """
+    def __init__(self, alpha=0.1, logger=None, wh=DTree, **wh_args):
         self.logger = logger = logging.getLogger(__name__)
         self.alpha = alpha
-        self.n_samples = n_samples
         self.wh = wh
         self.wh_args = wh_args
         self.P0 = 1
         self.P1 = 1
 
-    def fit_stage(self, model, X0, H0, X1, H1, theta=None):
+    def fit_stage(self, model, X0, H0, X1, H1, theta=None, **wh_args):
+        """ Append new stage to the model """
         W0 = weights(H0)
         W1 = weights(-H1)
-        weak = self.wh.fit(X0, W0, X1, W1, **self.wh_args)
+
+        weak = self.wh.fit(X0, W0, X1, W1, **{**self.wh_args, **wh_args})
 
         # Update H
         H0 = H0 + weak.predict(X0)
@@ -325,11 +135,11 @@ class Learner:
 
         model.append(weak, theta)
 
+        return loss(H0, H1), self.P0, self.P1
+
 
 def fit_rejection_threshold(H0, P0, H1, P1, alpha):
-    """
-    Fit threshold according to SPRT.
-    """
+    """ Fit threshold according to SPRT. """
     max0 = np.max(H0)
     min1 = np.min(H1)
     if max0 < min1:

waldboost/utils.py
@@ -69,6 +69,10 @@ def draw_detections(img, model, v=None, nms=False, min_score=None, v0=None, v1=N
     else:
         bbs,scores = detect_and_verify(img, model, v)
 
+    I = cv2.cvtColor(img[...,0], cv2.COLOR_GRAY2BGR)
+    if scores.size == 0:
+        return I
+
     if v0 is None:
         v0, v1 = scores.min(), scores.max()
 
@@ -81,7 +85,7 @@ def draw_detections(img, model, v=None, nms=False, min_score=None, v0=None, v1=N
 
     N = mpl.colors.Normalize(vmin=v0, vmax=v1)
     colors = cm.plasma;
-    I = cv2.cvtColor(img[...,0], cv2.COLOR_GRAY2BGR)
+
     order = np.argsort(scores)
     for (x,y,w,h),score in zip(bbs[order].astype("i"), scores[order]):
         clr = (255*np.array(colors(N(score)))[2::-1]).astype("u1")