7b813e7eeb591fef207b41906bb09555b1ed6643
Roman Juranek <ijuranek@fit.vutbr.cz>
2019-08-16 17:27:37 +0200

    removed bbx requirement

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url = "",
     keywords = "object detection, waldboost",
     packages = ["waldboost"],
-    install_requires = ["numpy", "scikit-image", "scikit-learn", "scipy", "bbx", "protobuf", "numba", "opencv-python"],
+    install_requires = ["numpy", "scikit-image", "scikit-learn", "scipy", "protobuf", "numba", "opencv-python"],
     scripts = None,
     python_requires = ">=3.6",
     include_package_data = True,
