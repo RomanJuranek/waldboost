704fd253efbdf2e5c7073400a106cdf9bcf39ad4
Roman Juranek <ijuranek@fit.vutbr.cz>
2019-08-29 11:32:01 +0200

    model testing code

--- waldboost/testing.py
+++ waldboost/testing.py
@@ -16,27 +16,10 @@ from object_detection.utils.object_detection_evaluation import \
 from . import bbox
 
 
-def detect(image, model, iou_threshold=0.3, score_threshold=-10):
+def detection_dict(dt_boxes):
     """
-    Wrapper that produces detection in TF Object Detection API format
-
-    Input
-    -----
-    image : ndarray
-        Input image - whatever accepter by model.detect
-    model : list of models
-
-    Output
-    ------
-    dt_dict : dict
-        Dictionary with keys "detection_boxes", "detection_scores" and
-        "detection_classes" as required by ObjectDetectionEvaluator
+    Convert BoxList to detection dict
     """
-    dt_boxes = wb.detect_multiple(image, *model, separate=False)
-
-    dt_boxes = bbox.np_box_list_ops.non_max_suppression(dt_boxes,
-                                                        iou_threshold=iou_threshold,
-                                                        score_threshold=score_threshold)
     return {
         "detection_boxes": dt_boxes.get(),
         "detection_scores": dt_boxes.get_field("scores"),
@@ -44,21 +27,47 @@ def detect(image, model, iou_threshold=0.3, score_threshold=-10):
     }
 
 
-def evaluate_model(model, testing_images, label="object"):
-    """ Test the given model on testing images and return evaluation structure """
-    category_index = [{"id":1,"name":label}]
+def evaluate_model(model, testing_images, category_index=None, **detect_kws):
+    """ Test the given model on testing images and return evaluation structure
+
+    Inputs
+    ------
+    model : list of wb.Model
+    testing_images : generator
+    category_index : dict
+    detect_kws :
+        Allowed keyword arguments are:
+        * channel_opts
+        * response_scale
+        * separate
+        * iou_threshold,
+        * score_threshold
+        See wb.detect_multiple for details
+    """
+
+    if isinstance(model, wb.Model):
+        model = [model]
+    
+    if not isinstance(model, list):
+        raise ValueError("Model must be waldboost.Model or list of waldboost.Model")
+
+    if category_index is None:
+        category_index = [{"id":1,"name":"object"}]
+
     E = ObjectDetectionEvaluator(categories=category_index,
                                  evaluate_precision_recall=True,
                                  matching_iou_threshold=0.5,
                                  evaluate_corlocs=True)
+    
 
     for idx,(im,gt_boxes,filename) in enumerate(testing_images):
         logging.debug(f"Processing {filename}")
-        dt_dict = detect(im, model)
+        dt_boxes = wb.detect_multiple(im, *model, **detect_kws)
+        dt_dict = detection_dict(dt_boxes)
         gt_dict = {
             "groundtruth_boxes": gt_boxes.get(),
             "groundtruth_classes": np.ones(gt_boxes.num_boxes(),"i"),
-            "groundtruth_difficult": gt_boxes.get("ignore").flatten(),
+            "groundtruth_difficult": gt_boxes.get_field("ignore").flatten(),
         }
         E.add_single_ground_truth_image_info(idx, gt_dict)
         E.add_single_detected_image_info(idx, dt_dict)
